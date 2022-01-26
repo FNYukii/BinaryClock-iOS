a2c71bb2b4e4dafa61b746d63ede41fd041f465e
Yu357 <forestgreen14689@gmail.com>
2022-01-26 14:39:14 +0900

    グリッドの隙間などを調整

--- BinaryClock/ContentView.swift
+++ BinaryClock/ContentView.swift
@@ -9,19 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
         
-    var rows: [GridItem] = Array(repeating: .init(.fixed(50)), count: 4)
+//    var gridItems: [GridItem] = Array(repeating: .init(.fixed(52)), count: 4)
     
     var body: some View {
         
-        LazyHGrid(rows: rows, alignment: .center, spacing: 2) {
-            ForEach((1...24), id: \.self) { num in
-                ZStack {
-                    Rectangle().foregroundColor(.accentColor)
-                        .frame(width: 40, height: 40)
-                    Text("\(num)").foregroundColor(.white)
-                }
+        LazyHGrid(rows: Array(repeating: .init(.fixed(40)), count: 4), spacing: 6, content: {
+            ForEach(0..<24) { _ in
+                Color.accentColor
+                    .frame(width: 40)
             }
-        }
+        })
         .onAppear {
             Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
                 updateClock()
