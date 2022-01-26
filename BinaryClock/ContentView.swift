a861dd793299bbe4dfe5dd3791228e0e34f030fe
Yu357 <forestgreen14689@gmail.com>
2022-01-26 15:11:51 +0900

    hhmmss文字列を使って、各けたを二進数にした文字列を生成した

--- BinaryClock/ContentView.swift
+++ BinaryClock/ContentView.swift
@@ -33,12 +33,20 @@ struct ContentView: View {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "HHmmss", options: 0, locale: Locale(identifier: "ja_JP"))
         let hhmmssWithColon = dateFormatter.string(from: dt)
-        let hhmmss = hhmmssWithColon.replacingOccurrences(of: ":", with: "")
-        
-        // TODO: Create binary "hhhhhhhhmmmmmmmmssssssss" from "hhmmss"
+        let hhmmss: String = hhmmssWithColon.replacingOccurrences(of: ":", with: "")
+        print(hhmmss)
+
+        // Create binary "hhhhhhhhmmmmmmmmssssssss" from "hhmmss"
+        var binaryHhmmss = ""
+        for i in 0..<hhmmss.count {
+            let oneNumChar: String = String(hhmmss[hhmmss.index(hhmmss.startIndex, offsetBy: i)]) // "4" <- "451208"
+            let binaryStr: String  = String(Int(oneNumChar)!, radix: 2) // "100" <- "4"
+            let filledBinaryStr: String = String(format: "%04d", Int(binaryStr)!) // "0100" <- "100"
+            binaryHhmmss += filledBinaryStr
+        }
+        print(binaryHhmmss)
         
         // TODO: Create 24 squares
         
-        print(hhmmss)
     }
 }
