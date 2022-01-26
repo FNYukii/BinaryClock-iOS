e01467a2fb72baefd6ed76bad9ca86dea0e72e1f
Yu357 <forestgreen14689@gmail.com>
2022-01-26 11:45:06 +0900

    Initial Commit

---
+++ BinaryClock/BinaryClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryClockApp.swift
+//  BinaryClock
+//
+//  Created by Yu on 2022/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BinaryClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
